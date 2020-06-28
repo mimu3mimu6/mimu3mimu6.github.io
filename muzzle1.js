47b1aa7aabf3f571eefa3597bcec3c725ba2f0ae
mimu3mimu6 <63854533+mimu3mimu6@users.noreply.github.com>
2020-06-28 15:45:50 +0900

    Update muzzle1.js

--- muzzle1.js
+++ muzzle1.js
@@ -1,22 +1,17 @@
 $(function() {
-  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
-//   $('#language-wrapper').hover(
-//     function() {
-//       $('.language-text').fadeIn();
-//     },
-//     function() {
-//       $('.language-text').fadeOut();
-//     }
-//   );
-  
+
   
 $('#text').hide();
+  $("#hide-text").click(function() {
+  $("#text").show();
+});
 
-	$("#hide-text").click(function() {
-		$("#text").show();
-	});
 
-  
+ $(".thumbnail").on('mouseover touchend',function(){
+    var dataUrl = $(this).attr('data-url');
+    $("#main-image").attr('src',dataUrl);
+});
+
   
 });
 
