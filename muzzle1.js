46c6f28d54dec9fb7f7bcd74a1c18bfc83d3f660
mimu3mimu6 <63854533+mimu3mimu6@users.noreply.github.com>
2020-07-04 23:58:10 +0900

    Update muzzle1.js

--- muzzle1.js
+++ muzzle1.js
@@ -6,7 +6,14 @@ $('#text').hide();
   $("#text").show();
 });
 
-
+$('#text2').hide();
+  $("#hide-text").click(function() {
+  $("#text2").show();
+});
+  
+  
+  
+  
  $(".thumbnail").on('mouseover touchend',function(){
     var dataUrl = $(this).attr('data-url');
     $("#main_image").attr('src',dataUrl);
